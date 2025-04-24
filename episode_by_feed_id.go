6442f25c5d468101e7218c80c53b9797e83aaba5
james <jjgmckenzie@gmail.com>
2025-04-24 12:06:40 -0700

    updates getepisodebyID

--- episode_by_feed_id.go
+++ episode_by_feed_id.go
@@ -9,12 +9,18 @@ import (
 	"strconv"
 )
 
-func (c *Client) GetEpisodeByID(ctx context.Context, feedID episode.ID) (*Podcast, error) {
-	var response getPodcastResponse
+type getSingleEpisodeResponse struct {
+	Id          string  `json:"id"`
+	Episode     Episode `json:"episode"`
+	Description string  `json:"description"`
+}
+
+func (c *Client) GetEpisodeByID(ctx context.Context, feedID episode.ID) (*Episode, error) {
+	var response getSingleEpisodeResponse
 	params := url.Values{"id": {strconv.Itoa(int(feedID))}}
 	err := c.api.Get(ctx, "/episodes/byid", params, &response)
 	if err != nil {
 		return nil, err
 	}
-	return &response.Feed, nil
+	return &response.Episode, nil
 }
